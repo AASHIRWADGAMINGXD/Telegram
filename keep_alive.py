485fc1d2f6785b8188c435c8d222110196e336b4
AASHIRWADGAMINGXD <130493466+AASHIRWADGAMINGXD@users.noreply.github.com>
2025-11-30 16:37:27 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from threading import Thread
+import logging
+
+# Stop flask from spamming logs
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "<h1>Bot Zinda Hai Bhai! (System Online)</h1>"
+    return "<h1>Bot Zinda Hai! (System Online)</h1>"
 
 def run():
+    # Use port 8080 or defaults to 5000 if 8080 is busy
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
